57b603542cc23aab381bc1821ed321c3473134a9
MuhammadFaleh <74877253+MuhammadFaleh@users.noreply.github.com>
2025-11-23 17:51:33 +0300

    Delete e-commerce directory

--- e-commerce/src/main/java/com/capstone1/e_commerce/Repository/MerchantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone1.e_commerce.Repository;
-
-import com.capstone1.e_commerce.Model.Merchant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MerchantRepository extends JpaRepository<Merchant,Integer> {
-}
--- e-commerce/src/main/java/com/capstone1/e_commerce/Repository/MerchantStockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone1.e_commerce.Repository;
-
-import com.capstone1.e_commerce.Model.MerchantStock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MerchantStockRepository extends JpaRepository<MerchantStock,Integer> {
-}
